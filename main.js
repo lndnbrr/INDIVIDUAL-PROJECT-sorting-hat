323a692a20404e9b45a79456f08ac1025ce1cb7c
Landon Borrego <lndnborrrego@gmail.com>
2024-10-28 04:25:14 -0400

    I have created a branch called stepThreeFilterStudentsByHouse. This is a branch that completed a portion of step three on README.md
    This branch modifies index.html and main.js. I have modified index.html by changing line 31 to a form, and adding a new div with an id of filterButtons. I have modified main.js by adding required to line 25, even though it doesnt work. I have also changed my for of loop to a .map array method. I have also added a string to insert all of my filter buttons. I have also added a form reset. Lastly, I have  attempted to make a filter, but I am merging this branch to be able to get help on a discussion ticket.

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ const allHouses = [
   { idNum: 3, house: "Ravenclaw" },
   { idNum: 4, house: "Slytherin" },
 ];
-// submit create student
-// function to take student's name, house and id. This function needs to return an object to push onto the allStudents array.
 
 // ******************** //
 // ****** Step 1 ****** //
@@ -19,16 +17,15 @@ const allHouses = [
 
 // This event listener is waiting for a click found in the element with an id of 'introButton'. When the button is clicked, the element with an id of 'idForSortHat' will disappear, and we add formString to the DOM through the element with an id of 'placeHere'.
 document.getElementById("introButton").addEventListener("click", () => {
-  const insertToDiv = document.getElementById("placeHere");
+  const insertToForm = document.getElementById("placeHere");
   document.getElementById("idForSortHat").style.display = "none";
-  let formString = `<form>
+  let formString = ` 
   <div class="mb-3">
   <label for="studentname">What's Your Name?</label>
-  <input type ="text" name='name' id="nameOfStudent">
+  <input type ="text" name='name' id="nameOfStudent" required> 
   </div>
-    <button type="submit" class="btn btn-primary" id="subButton" onclick = 'submitClicked(event)' >Submit</button>
-  </form>`;
-  insertToDiv.innerHTML = formString;
+    <button type="submit" class="btn btn-primary" id="subButton" onclick = 'submitClicked(event)' >Submit</button>`;
+    insertToForm.innerHTML = formString;
 });
 
 // ******************** //
@@ -36,29 +33,64 @@ document.getElementById("introButton").addEventListener("click", () => {
 // ******************** //
 
 // This function holds all of the functions that will occur when we click the submit button in our form that we rendered to the DOM. addCurrentstudents() adds all of the students to the DOM through the div with id 'putAllStudentsHere'. assignRandomHouse() is our function that is a random house generator. createNewStudent() is our function that creates a new student(NOTE: the name will pop up in the console.log but it will appear as undefined on the card).
+
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
 const submitClicked = (e) => {
   e.preventDefault();
 
+
+  let buttonString = `<div class="houseButtons">
+    <button type="button" class="btn btn-dark" id="allHousesButton" onclick = "everyHouse()">All Houses</button>
+    <button type="button" class="btn btn-danger" id="gryffindorButton" onclick ="callGryffindorHouse()">Gryffindor</button>
+    <button type="button" class="btn btn-warning" id="hufflepuffButton">Hufflepuff</button>
+    <button type="button" class="btn btn-primary" id="ravenclawButton">Ravenclaw</button>
+    <button type="button" class="btn btn-success" id="slytherinButton">Slytherin</button>
+
+  </div>`;
+  document.getElementById("filterButtons").innerHTML = buttonString;
+
+
+  // const addCurrentstudents = () => {
+  //   let domString = "";
+  //   for (const student of allStudents) {
+  //     domString += `<div class="card">
+  //       <div class="card-body">
+  //         <p class="card-text">${student.studentName}</p>
+  //         <p class="card-text">${student.house}</p>
+  //       </div>
+  //     </div>`;
+  //   };
+  //   document.getElementById("putAllStudentsHere").innerHTML = domString;
+
+  // };
+
   const addCurrentstudents = () => {
     let domString = "";
-    for (const student of allStudents) {
-      domString += `<div class="card">
-        <div class="card-body">
-          <p class="card-text">${student.studentName}</p>
-          <p class="card-text">${student.house}</p>
-        </div>
-      </div>`;
-    }
+  
+  allStudents.map(function(cv) {
 
-    document.getElementById("putAllStudentsHere").innerHTML = domString;
-  };
+     domString += `<div class="card">
+            <div class="card-body">
+              <p class="card-text">${cv.studentName}</p>
+              <p class="card-text">${cv.house}</p>
+            </div>
+          </div>`;
+
+   return document.getElementById("putAllStudentsHere").innerHTML = domString;
+  });
+}
+
 
   const assignRandomHouse = () => {
     let randomNumGen = Math.floor(Math.random() * allHouses.length);
     return allHouses[randomNumGen].house;
   };
 
-  // const form = document.querySelector('form')
 
   const createNewStudent = () => {
     const newStudent = {
@@ -70,8 +102,48 @@ const submitClicked = (e) => {
 
     addCurrentstudents(allStudents);
 
-    // form.reset();
   };
   createNewStudent();
+
+  document.querySelector("form").reset();
+
   console.log(allStudents);
+
+
+
+  // THE CODE ABOVE WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+  const filter = (studentsArray, houseType) => {
+    const studentsFiltered = [];
+  
+  
+    for (const studentObj of studentsArray) {
+      if (studentObj.house === houseType) {
+        studentsFiltered.push(studentObj);
+      }
+    }
+  
+    return studentsFiltered;
+  };
+  
+// THIS FILTER SET UP FUNCTION WORKS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+  const everyHouse = () => {
+  
+      return addCurrentstudents(allStudents);
+    };
+  
+  console.log(everyHouse());
+  
+  
+  
+  const callGryffindorHouse = () => {
+      const gryffindorOnly = filter(allStudents, "Gryffindor");
+    return gryffindorOnly;
+  };
+   console.log(callGryffindorHouse())
+
 };
